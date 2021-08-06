4224a94e25e694e66f1a1f9b47ca7c48940687a3
Trevor Huey <battlepanda100@gmail.com>
2021-08-05 23:21:39 -0500

    set up firebase

--- android/app/src/main/java/com/recipethyme/MainActivity.java
+++ android/app/src/main/java/com/recipethyme/MainActivity.java
@@ -1,4 +1,4 @@
-package com.recipethyme;
+package co.recipethyme;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -6,8 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
